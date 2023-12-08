53814dcb60b063843f38d4e0e35d575dce298101
LinusW-HWR <s_widing21@stud.hwr-berlin.de>
2023-12-08 10:41:52 +0000

    Implementing irl support

--- scripts/init_pose.py
+++ scripts/init_pose.py
@@ -3,28 +3,109 @@
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+
+
+class Velocity:
+    def __init__(self, angular, linear):
+        self.angular = angular
+        self.linear = linear
+
+
+class Linear:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+class Angular:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+def update_twist(velocity, publisher):
+    twist = Twist()
+
+    twist.linear.y = velocity.linear.y
+    twist.linear.z = velocity.linear.z
+
+    twist.angular.x = velocity.angular.x
+    twist.angular.y = velocity.angular.y
+    twist.angular.z = velocity.angular.z
+
+    # Delay
+    rospy.sleep(1)
+
+    publisher.publish(twist)
+    print("Velocity changed!")
+
+def are_odoms_equal(odom1, odom2, tolerance=0.2):
+    """
+    Check if two odom objects are equal within a specified tolerance.
+
+    Parameters:
+    - odom1: The first odom object.
+    - odom2: The second odom object.
+    - tolerance: The allowed difference in coordinates.
+
+    Returns:
+    - True if the odom objects are equal within the specified tolerance, False otherwise.
+    """
+
+    # Check if the coordinates are within the allowed tolerance
+    x_diff = abs(odom1.pose.pose.orientation.x - odom2.pose.pose.orientation.x)
+    y_diff = abs(odom1.pose.pose.orientation.y - odom2.pose.pose.orientation.y)
+    z_diff = abs(odom1.pose.pose.orientation.z - odom2.pose.pose.orientation.z)
+
+    return x_diff <= tolerance and y_diff <= tolerance and z_diff <= tolerance
+
 
 # Node initialization
 rospy.init_node('init_pose')
-pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=1)
+initialpose_pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=1)
+cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
 # Construct message
 init_msg = PoseWithCovarianceStamped()
 init_msg.header.frame_id = "map"
 
-# Get initial pose from Gazebo
-odom_msg = rospy.wait_for_message('/odom', Odometry)
-init_msg.pose.pose.position.x = odom_msg.pose.pose.position.x
-init_msg.pose.pose.position.y = odom_msg.pose.pose.position.y
-init_msg.pose.pose.orientation.x = odom_msg.pose.pose.orientation.x
-init_msg.pose.pose.orientation.y = odom_msg.pose.pose.orientation.y
-init_msg.pose.pose.orientation.z = odom_msg.pose.pose.orientation.z
-init_msg.pose.pose.orientation.w = odom_msg.pose.pose.orientation.w
+init_msg.pose.pose.position.x = -1.97
+init_msg.pose.pose.position.y = -0.43
+init_msg.pose.pose.orientation.x = 0.0
+init_msg.pose.pose.orientation.y = 0.0
+init_msg.pose.pose.orientation.z = 0.00
+init_msg.pose.pose.orientation.w = 0.99
+init_msg.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.06853892326654787]
 
 # Delay
 rospy.sleep(1)
 
 # Publish message
 rospy.loginfo("setting initial pose")
-pub.publish(init_msg)
+initialpose_pub.publish(init_msg)
 rospy.loginfo("initial pose is set")
+
+current_loc = rospy.wait_for_message('/odom', Odometry)
+rospy.sleep(1)
+
+ang = Angular(z=0.3)
+lin = Linear()
+vel = Velocity(angular=ang, linear=lin)
+update_twist(vel, cmd_vel_pub)
+
+rospy.sleep(2)
+turned = False
+
+while not turned:
+    odom2 = rospy.wait_for_message('/odom', Odometry)
+    if are_odoms_equal(current_loc, odom2, tolerance=0.02):
+        ang = Angular()
+        lin = Linear()
+        vel = Velocity(angular=ang, linear=lin)
+        update_twist(vel, cmd_vel_pub)
+        turned = True
